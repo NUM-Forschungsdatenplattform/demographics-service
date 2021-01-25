54b722cf405e83a16a6f9e584886b18b2e156ce8
Alexander Karle <alexander.karle@vitasystems.de>
2021-01-25 21:06:48 +0100

    feat(NUM-962) authorization -added consent endpoint tests

--- src/main/java/de/vitagroup/num/service/AuthorizationService.java
+++ src/main/java/de/vitagroup/num/service/AuthorizationService.java
@@ -1,26 +1,21 @@
 package de.vitagroup.num.service;
 
-import ca.uhn.fhir.jpa.api.dao.DaoRegistry;
-import ca.uhn.fhir.jpa.searchparam.SearchParameterMap;
-import ca.uhn.fhir.rest.api.server.IBundleProvider;
-import ca.uhn.fhir.rest.param.ReferenceParam;
 import java.util.Map;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.hl7.fhir.r4.model.Consent;
 import org.hl7.fhir.r4.model.DateTimeType;
 import org.hl7.fhir.r4.model.Period;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component
+@Service
 @RequiredArgsConstructor
 public class AuthorizationService {
 
   private static final String ORGANIZATION_ID = "organization_id";
   private static final String PATIENT_ID = "patient_id";
-  private static final String PATIENT_REFERENCE_PREFIX = "Patient/";
 
-  private final DaoRegistry daoRegistry;
+  private final ConsentService consentService;
 
   public boolean checkIsAuthorized(String policyName, Map<String, Object> values) {
     if (policyName.equals("has_consent")) {
@@ -41,7 +36,7 @@ public class AuthorizationService {
       return false;
     }
 
-    Optional<Consent> consent = retrieveConsent(patientId.get().toString());
+    Optional<Consent> consent = consentService.retrieveConsent(patientId.get().toString());
     if (consent.isEmpty()) {
       return false;
     }
@@ -61,21 +56,5 @@ public class AuthorizationService {
       return hasConsent;
     }
     return false;
-  }
-
-  private Optional<Consent> retrieveConsent(String patientId) {
-    SearchParameterMap searchParameterMap = new SearchParameterMap();
-    ReferenceParam patientReference = new ReferenceParam(PATIENT_REFERENCE_PREFIX + patientId);
-    searchParameterMap.add(Consent.SP_PATIENT, patientReference);
-
-    IBundleProvider bundleProvider =
-      daoRegistry.getResourceDao(Consent.class).search(searchParameterMap);
-
-    if (bundleProvider.isEmpty()) {
-      return Optional.empty();
-    }
-
-    int bundleSize = bundleProvider.size();
-    return Optional.of((Consent) bundleProvider.getResources(bundleSize - 1, bundleSize).get(0));
   }
 }
