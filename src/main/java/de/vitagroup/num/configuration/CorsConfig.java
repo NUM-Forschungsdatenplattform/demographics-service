d3cb8f771cee5bcee4a1421643c3467468913273
Alexander Karle <alexander.karle@vitasystems.de>
2021-04-23 13:41:12 +0200

    fix(NUM-1543) added cors filter
    - Added missing cors filter allowing all origins
    - Updated dependencies

---
+++ src/main/java/de/vitagroup/num/configuration/CorsConfig.java
@@ -0,0 +1,30 @@
+package de.vitagroup.num.configuration;
+
+import java.util.Collections;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+@RequiredArgsConstructor
+public class CorsConfig {
+
+  private static final String BASE_PATH = "/**";
+  private static final String ALLOWED_ORIGINS = "*";
+
+  @Bean
+  public CorsFilter corsFilter() {
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    CorsConfiguration corsConfiguration = new CorsConfiguration();
+
+    corsConfiguration.setAllowCredentials(true);
+    corsConfiguration.setAllowedOriginPatterns(Collections.singletonList(ALLOWED_ORIGINS));
+    corsConfiguration.setAllowedMethods(Collections.singletonList(CorsConfiguration.ALL));
+    corsConfiguration.setAllowedHeaders(Collections.singletonList(CorsConfiguration.ALL));
+    source.registerCorsConfiguration(BASE_PATH, corsConfiguration);
+    return new CorsFilter(source);
+  }
+}
